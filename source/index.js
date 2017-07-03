57ea6bfca7bc9499bab503697951c703b18af796
rogerbf <roger@bergfalk.nu>
2017-07-03 18:19:48 +0200

    export get

--- source/index.js
+++ source/index.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const endpoints = require(`./api-endpoints`)
+const request = require(`./request`)
+
+module.exports = {
+  get: Object.keys(endpoints).reduce((bound, endpoint) =>
+    Object.assign(bound, { [endpoint]: request.bind(null, endpoints[endpoint]) }),
+    {}
+  )
+}
