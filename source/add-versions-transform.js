88e2005caaa762d8a2fdd6db35325f987cf16dbf
rogerbf <roger@bergfalk.nu>
2017-07-12 20:59:14 +0200

    merge objects

--- source/add-versions-transform.js
+++ source/add-versions-transform.js
@@ -5,7 +5,18 @@ module.exports = new Transform({
   transform: (episode, encoding, callback) => {
     Promise.all(episode.versions.map(({ id }) => video({ id })))
     .then(versions => {
-      callback(null, Object.assign({}, episode, { versions }))
+      callback(
+        null,
+        Object.assign(
+          {},
+          episode,
+          {
+            versions: episode.versions.reduce((acc, version, i) => {
+              return acc.concat(Object.assign({}, version, versions[i]))
+            }, [])
+          }
+        )
+      )
     })
     .catch(error => callback(error))
   },
