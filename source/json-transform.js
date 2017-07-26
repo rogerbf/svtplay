0ff70f385b73be8286d071403234907e2c5ad640
rogerbf <roger@bergfalk.nu>
2017-07-26 23:26:12 +0200

    delete

--- source/json-transform.js
+++
@@ -1,8 +0,0 @@
-const { Transform } = require(`readable-stream`)
-
-module.exports = new Transform({
-  transform: (episode, encoding, callback) => {
-    callback(null, JSON.stringify(episode, null, 2))
-  },
-  objectMode: true
-})
