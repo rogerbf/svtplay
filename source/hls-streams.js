6a961857d43e24a57e592dd7f43c52080e542c40
rogerbf <roger@bergfalk.nu>
2017-07-29 23:39:25 +0200

    parser

--- source/hls-streams.js
+++ source/hls-streams.js
@@ -1,4 +1,17 @@
+const parseAttributeList = line =>
+  line.slice(line.indexOf(`:`) + 1)
+  .split(/,(?=[A-Z])/)
+  .reduce((all, attributeValue) => {
+    const [ attribute, value ] = attributeValue.split(`=`)
+    return Object.assign(all, { [attribute]: value })
+  }, {})
+
 module.exports = (m3u8 = ``) =>
   m3u8.split(`\n`)
   .slice(2)
   .filter(line => line.length > 0)
+  .reduce((all, line) =>
+    /^#EXT-X-STREAM-INF/.test(line)
+    ? [ ...all, parseAttributeList(line) ]
+    : [ ...all.slice(0, -1), Object.assign({}, all.slice(-1).pop(), { URI: line }) ]
+  , [])
