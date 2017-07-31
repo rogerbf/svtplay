ad1826d37d4e57a3ef447a4ec910d64a75503055
rogerbf <roger@bergfalk.nu>
2017-07-31 21:15:42 +0200

    variable naming

--- source/hls-streams.js
+++ source/hls-streams.js
@@ -1,5 +1,5 @@
 const parseValue = (attribute, value) => {
-  const attributes = {
+  const attributeSpecificValueParsers = {
     RESOLUTION: value => {
       const [ x, y ] = value.split(`x`).map(v => parseInt(v, 10))
       return { x, y }
@@ -12,8 +12,8 @@ const parseValue = (attribute, value) => {
     .map(v => v.replace(/"/g, ``).split(`,`))
     .map(v => v.length === 1 ? v.pop() : v)
     .map(v =>
-      attributes.hasOwnProperty(attribute)
-      ? attributes[attribute](v)
+      attributeSpecificValueParsers.hasOwnProperty(attribute)
+      ? attributeSpecificValueParsers[attribute](v)
       : v
     )
     .pop()
@@ -35,6 +35,7 @@ module.exports = (m3u8 = ``) =>
   m3u8.split(`\n`)
   .slice(2)
   .filter(line => line.length > 0)
+  .map(line => line.trim())
   .reduce(
     (all, line) =>
       /^#EXT-X-STREAM-INF/.test(line)
