e7d7f7472ba0af3ce336391b5b1c1334fae8f8e7
rogerbf <roger@bergfalk.nu>
2017-08-02 23:37:19 +0200

    simplify

--- source/generate-filename.js
+++ source/generate-filename.js
@@ -1,8 +1,8 @@
 const titleType = {
   MOVIE: ({ programTitle, year }) =>
-    `${programTitle.replace(/\s/g, `.`)}.${year}.WEB-DL`,
+    `${programTitle.replace(/\s/g, `.`)}.${year}`,
   SERIES_OR_TV_SHOW: ({ programTitle, season, episodeNumber, year }) =>
-    `${programTitle.replace(/\s/g, `.`)}.${year}.S${season}E${episodeNumber}.WEB-DL`
+    `${programTitle.replace(/\s/g, `.`)}.${year}.S${season}E${episodeNumber}`
 }
 
 module.exports = video => {
