aeee765743c2229a1d4c0be29a03a31b243d6acf
rogerbf <roger@bergfalk.nu>
2017-08-02 23:39:04 +0200

    add version id

--- source/generate-filename.js
+++ source/generate-filename.js
@@ -1,13 +1,13 @@
 const titleType = {
-  MOVIE: ({ programTitle, year }) =>
-    `${programTitle.replace(/\s/g, `.`)}.${year}`,
-  SERIES_OR_TV_SHOW: ({ programTitle, season, episodeNumber, year }) =>
-    `${programTitle.replace(/\s/g, `.`)}.${year}.S${season}E${episodeNumber}`
+  MOVIE: ({ programTitle, year }, { id }) =>
+    `${programTitle.replace(/\s/g, `.`)}.${year}.${id}`,
+  SERIES_OR_TV_SHOW: ({ programTitle, season, episodeNumber, year }, { id }) =>
+    `${programTitle.replace(/\s/g, `.`)}.${year}.S${season}E${episodeNumber}.${id}`
 }
 
-module.exports = video => {
+module.exports = (video, version) => {
   try {
-    return titleType[video.titleType](video)
+    return titleType[video.titleType](video, version)
   } catch (error) {
     return video.id
   }
